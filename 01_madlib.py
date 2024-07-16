80d5d72b9ba8d84eb111a96ea51f7eabf4ff66bf
wpcompsci <87338777+wpcompsci@users.noreply.github.com>
2024-07-16 09:33:37 -0400

    added files

--- 01_madlib.py
+++ 01_madlib.py
@@ -3,5 +3,8 @@ adjective = input("Enter an adjective: ")
 noun_1 = input("Enter a noun: ")
 noun_2 = input("Enter a noun: ")
 
+input(f"Press Enter to see the Madlib.")
+
 print(f"A {noun_1} named {name} ate a {noun_2}.")
 print(f"{name} found the {noun_2} to be {adjective}.")
+
